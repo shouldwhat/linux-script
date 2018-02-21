746495693862579b09e17353dfa81e0b71923522
wonkkyuu <shouldwhat@gmail.com>
2018-02-21 19:49:12 +0900

    docker-compose 파일을 사용한 service stack 배포 테스트

--- docker/basic_cmd.sh
+++ docker/basic_cmd.sh
@@ -5,6 +5,9 @@ CONTAINER_ID=37390bad58b9
 REPO_NAME=get-started
 TAG_NAME=part2
 
+COMPOSE_FILE_NAME=docker-compose.yml
+STACK_NAME=getstartedlab
+
 # -p : Dockerfile 내에 EXPOST한 80포트를 4000포트로 리매핑하여 실행
 docker run -p 4000:80 $IMAGE_NAME
 
@@ -25,3 +28,20 @@ docker login
 docker tag $IMAGE_NAME $REPO_NAME:$TAG_NAME
 docker push $REPO_NAME:$TAG_NAME
 
+# docker stack 배포
+docker stack deploy -c $COMPOSE_FILE_NAME $STACK_NAME
+
+# 배포된 docker stack 목록
+docker service ls
+docker stack ls
+
+# 배포된 container 목록
+docker service ps $STACK_NAME
+docker container ls
+
+# 배포된 docker stack 삭제
+docker stack rm $STACK_NAME
+
+# docker swarm 로그인&로그아웃
+docker swarm init
+docker swarm leave --force
