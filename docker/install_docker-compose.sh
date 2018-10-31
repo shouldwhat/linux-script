023991e64822652f1c02a19b5a03d87197873d26
wonkkyuu <shouldwhat@gmail.com>
2018-10-31 21:49:37 +0900

    docker-compose 설치 스크립트 추가

---
+++ docker/install_docker-compose.sh
@@ -0,0 +1,5 @@
+#
+# https://docs.docker.com/compose/install/#install-compose
+#
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
