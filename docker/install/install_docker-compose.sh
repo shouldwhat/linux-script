d1b0e8635479e7111bcff34e1c29581e7f6acc20
wonkkyuu <shouldwhat@gmail.com>
2018-11-01 10:24:21 +0900

    docker-compose 설치 스크립트 보안

--- docker/install/install_docker-compose.sh
+++ docker/install/install_docker-compose.sh
@@ -1,5 +1,8 @@
 #
-# https://docs.docker.com/compose/install/#install-compose
+# Ref. https://docs.docker.com/compose/install/#install-compose
+# This script work on CentOS 7
 #
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
